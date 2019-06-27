34c5932111ce653f4d5c30ce4aa080d1f15ced39
John Strunk <jstrunk@redhat.com>
2019-06-27 09:26:29 -0400

    Upgrade packages if they exist
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- setup-env.sh
+++ setup-env.sh
@@ -6,6 +6,6 @@ python3 -m venv "$VENV_NAME"
 source "$VENV_NAME/bin/activate"
 
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install --upgrade -r requirements.txt
 
 echo "Activate: . ./.venv/bin/activate"
